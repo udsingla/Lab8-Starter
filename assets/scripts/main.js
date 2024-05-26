3a03dbb1fb2770fecf291ff691e4355bda8bf241
udsingla <130015308+udsingla@users.noreply.github.com>
2024-05-26 04:53:08 -0700

    Completed Expose and half of explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,27 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if('serviceWorker' in navigator){
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', () => {
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      // B4. TODO - Once the service worker has been successfully registered, console
+      //            log that it was successful.
+      navigator.serviceWorker.register("./sw.js").then(function() {
+        console.log("service registered");
+      })
+
+      // B5. TODO - In the event that the service worker registration fails, console
+      //            log that it has failed.
+      .catch(function(){
+        console.log("service not registered and gave error.");
+      });
+      // STEPS B6 ONWARDS WILL BE IN /sw.js
+    });
+  }
+  
 }
 
 /**
@@ -111,6 +123,14 @@ async function getRecipes() {
         let jsonRecipe = await recipeUrl.json();
         // A8. TODO - Add the new recipe to the recipes array
         recipes.push(jsonRecipe);
+        // A9. TODO - Check to see if you have finished retrieving all of the recipes,
+        //            if you have, then save the recipes to storage using the function
+        //            we have provided. Then, pass the recipes array to the Promise's
+        //            resolve() method.
+        if(recipes.length == RECIPE_URLS.length){
+          saveRecipesToStorage(recipes);
+          resolve(recipes);
+        }
   
       }
       catch(err){
@@ -121,12 +141,7 @@ async function getRecipes() {
       }
   
     }
-    // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-    //            if you have, then save the recipes to storage using the function
-    //            we have provided. Then, pass the recipes array to the Promise's
-    //            resolve() method.
-    saveRecipesToStorage(recipes);
-    resolve(recipes);
+    
   });
 }
 
